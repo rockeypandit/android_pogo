f67ebe57719d1cb73360e367397f937792e52492
Rockey <rockey.pandit98@gmail.com>
2018-07-23 08:20:16 +0530

    Initial commit

---
+++ app/src/main/java/com/example/rockeypandit/pogo/ArrayAdapters.java
@@ -0,0 +1,56 @@
+package com.example.rockeypandit.pogo;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class ArrayAdapters extends ArrayAdapter<Cards> {
+
+    Context context;
+/*
+    public Context getContext() {
+        return context;
+    }
+
+    public void setContext(Context context) {
+        this.context = context;
+    }
+*/
+    public ArrayAdapters(Context context, int resourceId , List<Cards> items){
+        super(context,resourceId,items);
+
+
+
+    }
+    public View getView(int position, View convertView, ViewGroup parent){
+        Cards cardItem = getItem(position);
+        if(convertView == null)
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.item,parent,false);
+
+        TextView name = (TextView) convertView.findViewById(R.id.name);
+        ImageView image = (ImageView) convertView.findViewById(R.id.image);
+
+        name.setText(cardItem.getName());
+        image.setImageResource(R.mipmap.ic_launcher);
+
+        return convertView;
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+}
