4a1a9efd06e62960d40141f10c3d803b8bc64563
Rockey <rockey.pandit98@gmail.com>
2018-08-20 01:07:54 +0530

    complete youtube tinder

--- app/src/main/java/com/example/rockeypandit/pogo/Chat/ChatActivity.java
+++ app/src/main/java/com/example/rockeypandit/pogo/Chat/ChatActivity.java
@@ -1,6 +1,7 @@
 package com.example.rockeypandit.pogo.Chat;
 
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -14,6 +15,7 @@ import com.example.rockeypandit.pogo.Matches.MatchesAdapter;
 import com.example.rockeypandit.pogo.Matches.MatchesObject;
 import com.example.rockeypandit.pogo.R;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -45,12 +47,14 @@ public class ChatActivity extends AppCompatActivity {
         currentUserId = FirebaseAuth.getInstance().getCurrentUser().getUid();
 
 
-        mDatabaseUser = FirebaseDatabase.getInstance().getReference().child("User").child(currentUserId).child("connections").child("matches").child(matchId).child("chatId");
+        mDatabaseUser = FirebaseDatabase.getInstance().getReference().child("User").child(currentUserId).child("connections").child("matches").child(matchId).child("ChatId");
         mDatabaseChat = FirebaseDatabase.getInstance().getReference().child("Chat");
+
         getChatId();
+
         mRecyclerView = (RecyclerView) findViewById(R.id.recyclerView);
         mRecyclerView.setNestedScrollingEnabled(false);
-        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.setHasFixedSize(false);
         mChatLayoutManager = new LinearLayoutManager(ChatActivity.this);
         mRecyclerView.setLayoutManager(mChatLayoutManager);
         mChatAdapter = new ChatAdapter(getDataSetChat(),ChatActivity.this);
@@ -95,6 +99,8 @@ public class ChatActivity extends AppCompatActivity {
                 if(dataSnapshot.exists()){
                     chatId = dataSnapshot.getValue().toString();
                     mDatabaseChat = mDatabaseChat.child(chatId);
+                    getChatMessages();
+                    
                 }
             }
 
@@ -103,6 +109,52 @@ public class ChatActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    private void getChatMessages() {
+        mDatabaseChat.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                if(dataSnapshot.exists()){
+                    String message = null;
+                    String createdByUser = null;
+
+                    if(dataSnapshot.child("text").getValue()!=null){
+                        message = dataSnapshot.child("text").getValue().toString();
+                    }
+                    if(dataSnapshot.child("createdByUser").getValue()!=null){
+                        createdByUser = dataSnapshot.child("createdByUser").getValue().toString();
+                    }
+
+                    if(message!=null && createdByUser!=null){
+                        Boolean currentUserBoolean= false;
+                        if (createdByUser.equals(currentUserId)){
+                            currentUserBoolean = true;
+                        }
+                        ChatObject newMessage = new ChatObject(message,currentUserBoolean);
+                        resultsChat.add(newMessage);
+                        mChatAdapter.notifyDataSetChanged();
+
+
+                    }
+                }
+
+
+            }
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+            }
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+            }
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+            }
+        });
+
     }
 
     private ArrayList<ChatObject> resultsChat= new ArrayList<ChatObject>();
--- app/src/main/java/com/example/rockeypandit/pogo/Chat/ChatViewHolders.java
+++ app/src/main/java/com/example/rockeypandit/pogo/Chat/ChatViewHolders.java
@@ -5,15 +5,21 @@ import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.example.rockeypandit.pogo.R;
 
 public class ChatViewHolders extends RecyclerView.ViewHolder implements View.OnClickListener {
 
+    public TextView mMessage;
+    public LinearLayout mContainer;
     public ChatViewHolders(View itemView) {
         super(itemView);
         itemView.setOnClickListener(this);
+
+        mMessage = itemView.findViewById(R.id.message);
+        mContainer=itemView.findViewById(R.id.container);
 
         }
 
